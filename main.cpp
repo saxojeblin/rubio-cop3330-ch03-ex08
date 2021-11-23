84c37a517e6c3c35b7d8587f7a8ac56c9770ee0c
saxojeblin <dynamiicnr@gmail.com>
2021-11-23 15:01:38 -0500

    complete

--- main.cpp
+++ main.cpp
@@ -2,9 +2,24 @@
 
 using namespace std;
 
-int main() {
+int main() 
+{
+    int number;
+    string result;
 
-    cout << "aasdfasdfadf" << endl;
+    cout << "Please enter a whole number: ";
+    cin >> number;
+
+    if ((number % 2) == 0)  
+    {
+        result = "even";
+    }
+    else if ((number % 2) == 1) 
+    {
+        result = "odd";
+    }
+    
+    cout << "The value " << number << " is an " << result << " number." << endl;
 
     return 0;
 }
